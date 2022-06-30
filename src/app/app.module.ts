7a338f8d8142ffa75e8f42ee3fe9d7eb670f5425
ScriptBytes <scriptbytes.io@gmail.com>
2022-06-29 21:16:15 -0600

    Added template files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DemoComponent } from './core/pages/demo/demo.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DemoComponent
   ],
   imports: [
     BrowserModule,
